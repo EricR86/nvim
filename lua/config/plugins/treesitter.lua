56d59d390b8d0323cf93cac0537d653457c5faaf
Eric Roberts <notthebmovieactor@gmail.com>
2024-12-19 18:04:37 -0500

    Disable treesitter for very large files

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,36 +1,44 @@
+---@diagnostic disable: missing-fields
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "c", "elixir", "lua", "python", "rust", "bash", "vim",
-                "query",
-            },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- A list of parser names, or "all"
+      ensure_installed = {},
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+      auto_install = true,
 
-            indent = {
-                enable = true
-            },
+      indent = {
+        enable = true
+      },
 
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
+      ignore_install = {},
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
-    end
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- Disable treesitter highlighting on very large files (> 100 kB)
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false
+      },
+    })
+  end
 }
-
