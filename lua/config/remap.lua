d29629a44afd739f346db2d0bb5c81585e62897c
Eric Roberts <notthebmovieactor@gmail.com>
2024-12-20 14:55:20 -0500

    Add descriptions to mappings

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,11 +1,11 @@
 vim.g.mapleader = " "
 
--- Project view with netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Project view with file explorer (oil/netrw)
+vim.keymap.set("n", "<leader>pv", ":e .<cr>",{ desc="Project file view" })
 
--- Block moves text in and out of sections
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Block moves text in and out of section
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc="Move block of code up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc="Move block of code down" })
 
 -- Motions keeping cursor the same
 vim.keymap.set("n", "J", "mzJ`z")
@@ -15,11 +15,11 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Keep pasted text preserved
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc="Paste keeping preserved" })
 
 -- Select system clipboard register
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- selection
-vim.keymap.set("n", "<leader>Y", [["+Y]]) -- whole line
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to clipboard" }) -- selection
+vim.keymap.set("n", "<leader>Y", [["+Y]], {desc = "Yank line to clipboard" }) -- whole line
 
 -- Unbind this garbage
 vim.keymap.set("n", "Q", "<nop>")
@@ -31,7 +31,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Execute lua code under cursor
-vim.keymap.set("v", "<leader>x", ":lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Execute lua under cursor" })
 
 -- Leave terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
@@ -50,7 +50,9 @@ vim.keymap.set("n", "<leader>t", function()
     vim.api.nvim_win_set_height(0, TERM_HEIGHT)
     -- Fix the height so it does not get resized (local to window)
     vim.opt.winfixheight = true
-end)
+end,
+{ desc = "Open terminal bottom right" })
 
 -- Replace current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+{ desc = "Replace current word" })
