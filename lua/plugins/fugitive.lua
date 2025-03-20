0367890cfdb46a92c18d8c3570de759cacc32d98
Eric Roberts <notthebmovieactor@gmail.com>
2025-03-20 12:16:50 -0400

    Remove fullscreen fugitive from mapping

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>gs", function()
       vim.cmd("Git")
-      vim.cmd("only")
     end,
     { desc = "Open Fugutive Git Buffer" });
   end
